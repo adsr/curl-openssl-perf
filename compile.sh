919d860526e0f01016a0ea62d3d66b153b066aa1
Adam Saponara <as@etsy.com>
2023-09-27 15:46:43 -0400

    wip

--- compile.sh
+++ compile.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
+set -euo pipefail
+
+which=${1:-all}
 root=$(pwd)
 
-for openssl in opensslv1 opensslv3; do
-    pushd "$openssl"
-    git clean -fdx
-    CFLAGS='-g -O0' ./config --prefix="${root}/install-${openssl}" --libdir="${root}/install-${openssl}/lib"
-    make -j "$(nproc)" && make install
-    popd
+for openssl in opensslv1 opensslv3 opensslhead; do
+    if [ "$which" = all ] || [ "$which" = openssl ]; then
+        pushd "$openssl"
+        git clean -fdx
+        CFLAGS='-g -O0' ./config --prefix="${root}/install-${openssl}" --libdir="${root}/install-${openssl}/lib" --debug enable-ssl-trace
+        make -j "$(nproc)" && make install
+        popd
+    fi
 
-    pushd curl
-    git clean -fdx
-    autoreconf -fi && PKG_CONFIG_PATH="${root}/install-${openssl}/lib/pkgconfig/" CFLAGS='-g -O0' ./configure --with-openssl --prefix="${root}/install-curl-${openssl}"
-    make -j "$(nproc)" && make install
-    popd
+    if [ "$which" = all ] || [ "$which" = curl ]; then
+        pushd curl
+        git clean -fdx
+        autoreconf -fi && PKG_CONFIG_PATH="${root}/install-${openssl}/lib/pkgconfig/" CFLAGS='-g -O0' ./configure --with-openssl --prefix="${root}/install-curl-${openssl}"
+        make -j "$(nproc)" && make install
+        popd
+    fi
 
-    gcc \
-        -L"${root}/install-${openssl}/lib" \
-        -L"${root}/install-curl-${openssl}/lib" \
-        -Wl,--disable-new-dtags \
-        -Wl,-rpath="${root}/install-${openssl}/lib:${root}/install-curl-${openssl}/lib" \
-        curl-test.c \
-        -lcurl \
-        -o "curl-test-${openssl}"
+    if [ "$which" = all ] || [ "$which" = curl-test ]; then
+        gcc \
+            -L"${root}/install-${openssl}/lib" \
+            -L"${root}/install-curl-${openssl}/lib" \
+            -Wl,--disable-new-dtags \
+            -Wl,-rpath="${root}/install-${openssl}/lib:${root}/install-curl-${openssl}/lib" \
+            curl-test.c \
+            -lcurl \
+            -o "curl-test-${openssl}"
+    fi
 done
--- curl-test.c
+++ curl-test.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 #include <curl/curl.h>
 
+
+static size_t cb(char *ptr, size_t size, size_t nmemb, void *userdata) {
+    return size * nmemb;
+}
+
 int main(int argc, char **argv) {
     CURLcode res;
 
@@ -14,12 +19,17 @@ int main(int argc, char **argv) {
         CURL *curl = curl_easy_init();
         curl_easy_setopt(curl, CURLOPT_URL, "https://localhost:8181/");
         curl_easy_setopt(curl, CURLOPT_CAINFO, "./cert/test.crt");
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, cb);
         CURLcode res = curl_easy_perform(curl);
         if (res != CURLE_OK) {
             fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
             break;
         }
+        double t_total, t_pretransfer;
+        curl_easy_getinfo(curl, CURLINFO_TOTAL_TIME, &t_total);
+        curl_easy_getinfo(curl, CURLINFO_PRETRANSFER_TIME, &t_pretransfer);
         curl_easy_cleanup(curl);
+        printf("%.7f %.7f\n", t_total, t_pretransfer);
     }
 
     curl_global_cleanup();
